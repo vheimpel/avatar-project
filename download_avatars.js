f4c4ecb56a25f6baad68cbffa6da9771cd25166f
vheimpel <vheimpel@gmail.com>
2017-05-09 23:06:13 +0000

    Working

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,21 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "vheimpel";
 var GITHUB_TOKEN = "40b24ddf955eb70bdda4cb2f9368deebcfa33459";
+var userUrl = "";
+var userLogin = "";
 
+
+function downloadImageByURL(url, userName) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(`avatars/${userName}.jpg`));
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -18,15 +32,15 @@ function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
     info.forEach(function(user) {
-      console.log(user.avatar_url);
+      userAvatarUrl = user.avatar_url
+      userLogin = user.login
+      downloadImageByURL(userAvatarUrl, userLogin)
     });
 
   }
 }
 
-function downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg") {
-}
-
 getRepoContributors("jquery", "jquery", callback)
+
 
 
