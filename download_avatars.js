8bd4b302b28ea0c37782947665c87f695a40177b
vheimpel <vheimpel@gmail.com>
2017-05-09 23:20:10 +0000

    With comments

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,10 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "vheimpel";
 var GITHUB_TOKEN = "40b24ddf955eb70bdda4cb2f9368deebcfa33459";
-var userUrl = "";
+var userAvatarUrl = "";
 var userLogin = "";
 
-
+//Downloads the images, names them using the username
 function downloadImageByURL(url, userName) {
   request.get(url)
        .on('error', function (err) {
@@ -17,6 +17,7 @@ function downloadImageByURL(url, userName) {
        .pipe(fs.createWriteStream(`avatars/${userName}.jpg`));
 }
 
+//Concatinates the url where the list of repo contributors are found
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
     var options = {
@@ -28,6 +29,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, cb);
 }
 
+//Iterates through the avatar urls and user names and stores them in userAvatarUrl and userLogin
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
@@ -36,7 +38,6 @@ function callback(error, response, body) {
       userLogin = user.login
       downloadImageByURL(userAvatarUrl, userLogin)
     });
-
   }
 }
 
